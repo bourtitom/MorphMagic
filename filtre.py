e240856171f5784888c6c5a77eac9ce4cf095fd7
Mitake <kev800da@gmail.com>
2023-11-22 09:25:57 +0100

    ajout d'une fonction de floutage d'image

--- filtre.py
+++ filtre.py
@@ -5,6 +5,7 @@ importation des bibliothèque opencv et matplotlib
 
 import cv2 
 import numpy as np
+import os
 
 '''
 lienImage : variable contenant le lien vers l'iame à modifier
@@ -17,6 +18,21 @@ def filterImgBTW():
 
     img = cv2.imread(lienImage, cv2.IMREAD_GRAYSCALE)
     cv2.imwrite(f'img/{nameNewImg}', img)
-    
+   
 
 filterImgBTW()
+ 
+'''
+Fonction pour flouter l'image donnée
+On enregistre l'image dans la variable img en appliquant le filtre de niveau de floutage, et on l'enregistre
+'''
+def FilterFlouImg():
+    lienImage = str(input("Entrez le lien de l'image à flouter : "))
+    nameNewImg = str(input("Entrez le nom de votre nouvelle image : "))
+
+    img = cv2.imread(lienImage)
+    blurred_img = cv2.GaussianBlur(img, (15, 15), 0)
+    cv2.imwrite(f'img/{nameNewImg}', blurred_img)
+
+FilterFlouImg()
+
