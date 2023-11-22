ac2e94e125b903d7ee55378a7049e0dbdc6f9192
tenneguin nicolas <nicolas.tenneguin@gmail.com>
2023-11-22 13:36:54 +0100

    ajout fonction unique story7

--- filtre.py
+++ filtre.py
@@ -13,78 +13,121 @@ lienImage : variable contenant le lien vers l'iame à modifier
 On enregistre l'image dans la variable img en appliquant le filtre de niveau de gris, on l'affiche et on l'enregistre
 '''
 
-def filtreDilate():
-    lienImage = str(input("entrer le lien de l'image à mettre en noir et blanc : "))
-    nameNewImg = str(input("entrer le nom de votre nouvelle image : "))
+def filtreDilate(img):
     kernel = np.ones((5, 5), np.uint8)
-    # matrice 5px * 5px permettant de savoir si l'on doit étendre la couleur ou non
-    #uint8 : entier non signé sur 8 bit, c'est utilisé par numpy dans le cadre de traitement d'image
-    img = cv2.imread(lienImage, 1)
     img = cv2.dilate(img, kernel, iterations=1) # fonction d'opencv permettant la dilatation
-    cv2.imwrite(f'img/{nameNewImg}', img)
+    return img
 
-def filterImgBTW():
-    lienImage = str(input("entrer le lien de l'image à mettre en noir et blanc : "))
-    nameNewImg = str(input("entrer le nom de votre nouvelle image : "))
 
-    img = cv2.imread(lienImage, cv2.IMREAD_GRAYSCALE)
-    cv2.imwrite(f'img/{nameNewImg}', img)
+def filterImgBTW(img):
+    img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("fenetre3", img2)
+    cv2.waitKey(0)
+    return img2
 
 '''
 Fonction pour flouter l'image donnée
 On enregistre l'image dans la variable img en appliquant le filtre de niveau de floutage, et on l'enregistre
 '''
-def FilterFlouImg():
-    lienImage = str(input("Entrez le lien de l'image à flouter : "))
-    nameNewImg = str(input("Entrez le nom de votre nouvelle image : "))
-
-    img = cv2.imread(lienImage)
+def FilterFlouImg(img):
     blurred_img = cv2.GaussianBlur(img, (15, 15), 0)
-    cv2.imwrite(f'img/{nameNewImg}', blurred_img)
+    return blurred_img
 
-def rotateImg():
-    lienImage = str(input("Entrer le chemin de l'image à pivoter : "))
+def rotateImg(image):
     degreRotate = int(input("Entrer le nombre de degrés de rotation souhaité : "))
-    nameNewImg = str(input("Entrer le nom de votre nouvelle image : "))
-
-    image = cv2.imread(lienImage) ## charge l'image qui viens de lienImage
-
     (h, w) = image.shape[:2]
     cX, cY = w // 2, h // 2  ## recupe les dimensions de "image"
-
     M = cv2.getRotationMatrix2D((cX, cY), degreRotate, 1.0) # créer la matrice de rotation
-
     rotated = cv2.warpAffine(image, M, (w, h)) # rotate img 
-    cv2.imwrite(f'img/{nameNewImg}', rotated)
+    return rotated
 
-def redimImg():
-    lienImage = str(input("Entrer le chemin de l'image à redimensioner : "))
+def redimImg(image):
     newSize = float(input("entrer le facteur de redimensionnement de l'image :"))
-    image = cv2.imread(lienImage)
     largeur = int(image.shape[1] * newSize / 100)
     hauteur = int(image.shape[0] * newSize / 100)
     dimension = (largeur, hauteur)
     image=cv2.resize(image,dimension, interpolation = cv2.INTER_AREA)
-    nameNewImg = str(input("Entrer le nom de votre nouvelle image : "))
-    cv2.imwrite(f'img/{nameNewImg}', image)
+    return image
  
 
-def writeImg():
-    lienImage = str(input("Entrer le chemin de l'image : "))
+def writeImg(image):
     TxtImage = str(input("Entrer le texte de l'image : "))
-    nameNewImg = str(input("Entrer le nom de votre nouvelle image : "))
-
-    image = cv2.imread(lienImage)
-
-    ## proprietie du texte sur l'image
     position = (10, 50)
     font = cv2.FONT_HERSHEY_SIMPLEX
     fontScale = 1
     fontColor = (255, 255, 0)
     thickness = 2
-
-    #on ecrit le texte sur l'image avec toute les proprietés
     writed = cv2.putText(image, TxtImage, position, font, fontScale, fontColor, thickness)
+    return writed
 
-    cv2.imwrite(f'img/{nameNewImg}', writed)
+def filterAll():
+    test = 0
+    listeImg = []
+    while test == 0:
+        lienImage = str(input("Entrer le chemin de l'image : "))
+        
+        if lienImage == "":
+            test = 1
 
+        else:
+            img = cv2.imread(lienImage, 1) # transformation en noir et blanc
+            listeImg.append(img)
+    nombreImg = len(listeImg)
+    
+    while test < 7:
+        print("si vous voulez appliquez le filtre noir et blanc, taper 1")
+        print("si vous voulez appliquez le filtre flou, taper 2")
+        print("si vous voulez appliquez le filtre dilate, taper 3")
+        print("si vous voulez appliquez le filtre rotate, taper 4")
+        print("si vous voulez appliquez le filtre resize, taper 5")
+        print("si vous voulez appliquez le filtre rewrite, taper 6")
+        print("si vous voulez enregistrer votre image, taper 7")
+        test = int(input("entrer votre choix : "))
+        if test == 1:
+            for i in range (nombreImg):
+                image = listeImg[i]
+                image = filterImgBTW(image)
+                listeImg[i] = image
+                
+        if test == 2:
+            for i in range (nombreImg):
+                image = listeImg[i]
+                image = FilterFlouImg(image)
+                listeImg[i] = image
+                
+        if test == 3:
+            for i in range (nombreImg):
+                image = listeImg[i]
+                filtreDilate(image)
+                listeImg[i] = image
+                
+        if test == 4:
+            for i in range (nombreImg):
+                image = listeImg[i]
+                image = rotateImg(image)
+                listeImg[i] = image
+                
+        if test == 5:
+            for i in range (nombreImg):
+                image = listeImg[i]
+                image = redimImg(image)
+                listeImg[i] = image
+                
+        if test == 6:
+            for i in range (nombreImg):
+                image = listeImg[i]
+                image = writeImg(image)
+                listeImg[i] = image
+                
+        if test == 7:
+            nom_dossier = str(input("entrer le nom du dossier dans lequelle vous voulez enregistrer vos images : "))
+            nameNewImg = str(input("Entrer le nom de votre nouvelle image : "))
+            os.mkdir(nom_dossier)
+            for i in range (nombreImg):
+                image = listeImg[i]
+                nb = str(i)
+
+                cv2.imwrite(f'{nom_dossier}/{nameNewImg}{nb}.png', image)
+        
+
+filterAll()
